e28b3816070b07dad08db4b8fe838ad408b60f7e
Shivanshu Kashyap <44648459+kriyptor@users.noreply.github.com>
2021-05-14 01:06:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let weather = {
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=" + this.apiKey)
     .then((response) => response.json())
     .then((data) => this.displayWeather(data));
+    .catch(err => aler("Kindly enter a valid city name!"));
     },
     displayWeather : function(data){
         const { name } = data;
